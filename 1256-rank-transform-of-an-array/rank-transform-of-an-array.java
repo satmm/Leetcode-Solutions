9eb6e5d4fda2dd73c0e4f7dcc6cbec5465b40d80
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-10-02 20:52:13 +0530

    Time: 100 ms (5.21%) | Memory: 61.1 MB (77.36%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] arrayRankTransform(int[] a) {
+        // Use a TreeMap to automatically sort the values
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        // Store values in the TreeMap
+        for (int val : a) {
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+
+        // Assign ranks to the values
+        int rank = 1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            map.put(entry.getKey(), rank);
+            rank++;
+        }
+
+        // Traverse the array and assign ranks based on the map
+        int[] ans = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            ans[i] = map.get(a[i]);
+        }
+
+        // Return the ranks
+        return ans;
+    }
+}
