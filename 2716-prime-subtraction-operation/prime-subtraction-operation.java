1a7780c56a92755026ae5d9cc244a82bab5abfe8
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-11-11 13:36:52 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (33.77%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -1,32 +1,25 @@
 class Solution {
     public boolean primeSubOperation(int[] nums) {
-        int[] primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
-
-        int n=nums.length;
-        int l=0, h=0;
-        for(int i=n-2; i>=0; i--){
-            if(nums[i]>=nums[i+1]){
-                l=nums[i]-(nums[i+1]-1);
-                h=nums[i];
-                int p=getPrime(primes, l, h);
-                if(p==h){
-                    return false;
-                }
-                else{
-                    nums[i]=nums[i]-p;
-                }
+        int x=0;    //Initialize x as 0, which keeps track of the last modified value in nums
+        for (int num : nums){
+            if(x >= num) return false;
+            int prime = num-x-1;    // Calculate the maximum possible prime less than (num - x)
+            while(prime > 0 && !(isPrime(prime))){// Find the largest prime less than or equal to (num - x - 1)
+                prime--;     //Decrement `prime` until a prime number is found
             }
+            x = num-prime;   // Update x to the modified value of num (i.e., num - prime)
         }
-
         return true;
     }
 
-    int getPrime(int[] primes, int l, int h){
-        for(int i=0; i<primes.length; i++){
-            if(l<=primes[i] && primes[i]<h){
-                return primes[i];
-            }
+    boolean isPrime(int n){
+        if(n <= 1) return false;
+        if(n == 2) return true;
+        if(n%2 ==0 || n%3 == 0) return false;
+        // Check divisibility from numbers starting from 5, only checking odd numbers
+        for(int i=5; i*i<=n ; i+=6){    // Skip multiples of 2 and 3 by checking divisibility with i and i + 2
+            if(n % i == 0 || n % (i+2) == 0) return false;
         }
-        return h;
+        return true;
     }
 }
