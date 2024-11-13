b0a500af69630bb01b504cbd68420ef7879e1b37
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-11-13 20:31:07 +0530

    Time: 23 ms (100.00%) | Memory: 57.4 MB (26.26%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -0,0 +1,29 @@
+class Solution {
+     private long findLessThanEqualTo(int[] nums,int targetSum)
+    {
+       int n=nums.length;
+        long pairs=0;
+        int left=0;
+        int right=n-1;
+
+        while(left<right)
+        {
+            if(nums[left]+nums[right]<=targetSum)
+            {
+                pairs+=(right-left);
+                left++;
+            }
+            else
+            {
+                     right--;
+            }
+            
+        }
+        return pairs;}
+    public long countFairPairs(int[] nums, int lower, int upper) {
+         Arrays.sort(nums); 
+        long pairsWithUpper = findLessThanEqualTo(nums, upper);
+        long pairsWithLowerMinus1 = findLessThanEqualTo(nums, lower - 1);
+        return pairsWithUpper - pairsWithLowerMinus1;
+    }
+}
