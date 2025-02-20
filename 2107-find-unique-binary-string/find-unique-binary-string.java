fcdc233b9b35327418ccceb66041cb47bb1a8cf9
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2025-02-20 14:08:44 +0530

    Time: 3 ms (36.03%) | Memory: 41.6 MB (57.18%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+         int n = nums.length;
+        Set<String> set = new HashSet<>();
+
+        for (String num : nums) {
+            set.add(num);
+        }
+        for (int i = 0; i < (1 << n); i++) {
+            String binaryString = Integer.toBinaryString(i);
+            while (binaryString.length() < n) {
+                binaryString = "0" + binaryString;
+            }
+
+            if (!set.contains(binaryString)) {
+                return binaryString;
+            }
+        }
+         return "";
+    }
+}
